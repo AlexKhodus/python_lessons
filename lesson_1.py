f93903974282facc61075786a751046a2003788d
AlexKhodus <49063195+AlexKhodus@users.noreply.github.com>
2019-11-27 16:15:36 +0300

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+# def func_1(a):
+#     print(list(filter(lambda elem: elem < 5, a)))
+# func_1(a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
+# def func_2(a,b):
+#     result = list(filter(lambda elem: elem in b, a))
+#     print(result)
+# func_2([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+# def add(x, y):
+#     return x + y
+# print(add(4,5))
+# def func_3(dict_a, dict_b, dict_c):
+#     result = {**dict_a, **dict_b, **dict_c}
+#     print(result)
+# func_3({1: 10, 2: 20},{3: 30, 4: 40},{5: 50, 6: 60})
